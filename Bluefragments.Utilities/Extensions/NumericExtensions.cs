4c8f6cddaa34f6954c1c757ed2d83ff4c6786102
Thomas Martinsen <thomas@bluefragments.com>
2020-06-30 16:38:28 +0200

    Added FromNumericDateTimeValue as numeric extension.

--- Bluefragments.Utilities/Extensions/NumericExtensions.cs
+++ Bluefragments.Utilities/Extensions/NumericExtensions.cs
@@ -15,5 +15,36 @@ namespace Bluefragments.Utilities.Extensions
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return epoch.AddMilliseconds(unixTime);
         }
+
+        public static DateTime FromNumericDateTimeValue(this long value)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            try
+            {
+                return epoch.AddSeconds(value);
+            }
+            catch { }
+
+            try
+            {
+                return epoch.AddMilliseconds(value);
+            }
+            catch { }
+
+            try
+            {
+                return epoch.AddMilliseconds(value / 1000);
+            }
+            catch { }
+
+            try
+            {
+                return DateTime.FromFileTimeUtc(value);
+            }
+            catch { }
+
+            return DateTime.MinValue;
+        }
     }
 }
