613ee8fbf16eac7332b9fffcc121cf34a1f838f4
Ulrich Bodentien <ulbo@bluefragments.com>
2020-08-17 13:05:51 +0200

    Add sql client

---
+++ Bluefragments.Utilities.Data.SqlServer.Tests/TestDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bluefragments.Utilities.Data.SqlServer.Tests
+{
+    public class TestDatabaseContext : DbContext
+    {
+        public TestDatabaseContext()
+        {
+        }
+
+        public TestDatabaseContext(DbContextOptions<TestDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestEntity> Tests { get; set; }
+    }
+}
---
+++ Bluefragments.Utilities.Data.SqlServer/ISqlClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Bluefragments.Utilities.Data.SqlServer
+{
+    public interface ISqlClient
+    {
+        IDisposable CreateTransactionScope();
+
+        Task CreateItemAsync<T>(T entity)
+            where T : SqlDataEntity;
+
+        Task DeleteItemAsync<T>(string id)
+            where T : SqlDataEntity;
+
+        Task<T> GetItemAsync<T>(string id)
+            where T : SqlDataEntity;
+
+        Task<List<T>> GetItemsAsync<T>()
+            where T : SqlDataEntity;
+
+        Task<List<T>> GetItemsAsync<T>(Expression<Func<T, bool>> expression)
+            where T : SqlDataEntity;
+
+        Task ModifyItemAsync<T>(string id, Action<T> modifyItemAction)
+            where T : SqlDataEntity;
+
+        void UpdateItem<T>(T entity)
+            where T : SqlDataEntity;
+    }
+}
